e4aa0f88172130a8d352be1c5c018e33f133e3e6
Benjamin Simmers <bej1380@gmail.com>
2025-02-23 16:19:50 +1000

    simplify file content fetch

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,24 +1,21 @@
 import { CompendiumItem, File as fileResponse } from "../utils/types";
 
-const fetchFileContent = async (file: fileResponse): Promise<string> => {
-    const response = await fetch(file.url);
-    return response.text();
-  };
-  
-  const assignFilesToCompendium = async (
-    items: CompendiumItem[],
-    files: fileResponse[]
-  ) => {
-    const updatedItems = await Promise.all(
-      items.map(async (item) => {
-        const regex = new RegExp(`^${item.tabId}-`);
-        const file = files.find((f) => regex.test(f.path));
-        return file ? { ...item, content: await fetchFileContent(file) } : item;
-      })
-    );
-    return updatedItems;
-  };
-  
+const fetchFileContent = async (file: fileResponse): Promise<string> =>
+  (await fetch(file.url)).text();
+
+const assignFilesToCompendium = async (
+  items: CompendiumItem[],
+  files: fileResponse[]
+) => {
+  const updatedItems = await Promise.all(
+    items.map(async (item) => {
+      const regex = new RegExp(`^${item.tabId}-`);
+      const file = files.find((f) => regex.test(f.path));
+      return file ? { ...item, content: await fetchFileContent(file) } : item;
+    })
+  );
+  return updatedItems;
+};
 
 const buildCompendiumHierarchy = (items: CompendiumItem[]) => {
   const itemMap = new Map<number, CompendiumItem>();
